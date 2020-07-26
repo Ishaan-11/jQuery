55983a18f8782acf7ac45e28d02c69f37efe12ea
Ishaan-11 <ishaansharma1998@gmail.com>
2020-07-26 18:42:05 +0530

    13.5

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-/* $(document).ready(function() {
-    $("h1").css("color","red");
-}); */
+//set value to red of color property
+// $("h1").css("color","red");
 
-$("h1").css("color","red");
+//gives current value of property
+// console.log($("  h1").css("font-size"));
+
+// $("h1").addClass("big-title");
+
+$("h1").addClass("big-title margin-50");
